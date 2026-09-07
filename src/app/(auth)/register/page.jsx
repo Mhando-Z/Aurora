bb297222c6af2c73d81ffc82cccb66670c81e318
Mhando-Z <mhandosz17@gmail.com>
2026-09-07 20:55:26 +0300

    Phase One

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -27,7 +27,7 @@ export default async function RegisterPage({ searchParams }) {
   const error = params?.error;
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-50 px-6">
+    <main className="flex relative min-h-screen items-center justify-center bg-gray-50 px-6">
       <div className="w-full max-w-md">
         <div className="">
           {/* Brand logo */}
@@ -121,6 +121,28 @@ export default async function RegisterPage({ searchParams }) {
             Sign in
           </Link>
         </p>
+      </div>
+
+      {/* home button */}
+      <div className="absolute top-6 left-4  md:left-6 bg-black rounded-full px-3 p-2">
+        <Link href="/" className="text-white text-sm flex items-center gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h 8.25"
+            />
+          </svg>
+
+          <p>Home</p>
+        </Link>
       </div>
     </main>
   );
